4131fca43e0b1ee7e3f40503a21980d503d21866
Tom Ritchford <tom@swirly.com>
2023-02-10 12:47:37 +0100

    Much sketching around emojis

--- multi/tweak_index.py
+++ multi/tweak_index.py
@@ -33,14 +33,109 @@ def rewrite(path='/code/abbrev/.cache/gh-pages/index.html'):
     return soup
 
 
-def round_trip(s):
-    html = etree.HTML(s)
-    result = etree.tostring(html, pretty_print=True, method="html")
-    return result.decode()
+from lxml import etree
 
+
+def run(s):
+    html = etree.HTML(s)
+    assert html is not None, 'etree.HTML(s) returned None!'
+
+    return etree.tostring(html, pretty_print=True, method="html").decode()
+
+
+def round_trip(s):
+    print('Result:')
+    print(run(s))
+
+
+def old_compare_both():
+    p1 = '<html><head><title>ANT</title></head><body></body></html>'
+
+    # Works
+    round_trip(p1)
+    round_trip('\n' + p1)
+
+    p2 = """<html><head><title>🐜</title></head><body>
+휀휁휂휃휄휅휆휇휈😆</body></html>"""
+
+    # Works
+    round_trip(p2.encode())
+    round_trip(('\n' + p2).encode())
+
+    # Fails
+    round_trip(p2)         # Wrong answer
+    round_trip('\n' + p2)  # Returns None
+
+
+def compare_both():
+    cps = (
+        0x10000,
+        0x10001,
+        0x10002,
+        0x10003,
+
+        0x10010,
+        0x10020,
+        0x10030,
+        0x100FF,
+
+        0x10100,
+        0x10200,
+        0x10300,
+
+        0x11000,
+        0x12000,
+        0x13000,
+
+        0x20000,
+        0x30000,
+        0x40000,
+        0x80000,
+        0x100000,
+        0x100001,
+    )
+    cps = range(0x010000, 0x010100)
+
+    cps = range(0x80, 0x014000)
+
+    def cpt(cp):
+        try:
+            c = chr(cp)
+            c.encode()
+        except UnicodeEncodeError:
+            return ''
+        return f'0x{cp:06x}: {c}\n'
+
+    s = ''.join(cpt(cp) for cp in cps)
+
+    p1 = f'<html><head><title>test</title></head><body>\n{s}\n</body></html>'
+
+    s = run(p1.encode())
+
+    import re
+
+    pat = re.compile(r'0x(.*?): ' + r'(?:&#(\d\d\d);){1,4}\n')
+
+    diff = -1
+
+    def rep(m):
+        if True:
+            return m.group(0)
+        nonlocal diff
+
+        n, *parts = m.groups()
+        h = ''.join(hex(int(i))[2:] for i in parts)
+        before, after = int(n, 16), int(h, 16)
+        d2 = before - after
+        if d2 == diff:
+            return ''
+        diff = d2
+        return f'0x{n}: 0x{diff:x}\n'
+
+    print(pat.sub(rep, s))
 
 def rewrite2(path=Path('test-index.html')):
-    broken_html = "<!doctype html><html><head><title>test<body><h1>page title</h3>"
+    broken_html = "<html><head><title>test<body><h1>page title</h3>"
     print(round_trip(broken_html))
 
     source = path.read_text().strip()
@@ -64,4 +159,4 @@ def rewrite_lxml(path):
 
 
 if __name__ == '__main__':
-    rewrite2()
+    compare_both()
