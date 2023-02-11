396d29100e2ec546a2c716967850881b6a0a5c2d
Tom Ritchford <tom@swirly.com>
2023-02-11 13:35:41 +0100

    Work more on lxnl workaround

--- multi/tweak_index.py
+++ multi/tweak_index.py
@@ -24,15 +24,6 @@ def tweak_index(project, path):
     path.write_text(s3)
 
 
-def rewrite(path='/code/abbrev/.cache/gh-pages/index.html'):
-    from bs4 import BeautifulSoup
-
-    path = Path(path)
-    source = path.read_text().strip()
-    soup = BeautifulSoup(source, 'html.parser')
-    return soup
-
-
 from lxml import etree
 
 
@@ -45,7 +36,10 @@ def run(s):
 
 def round_trip(s):
     print('Result:')
-    print(run(s))
+    if not True:
+        print(run(s))
+    else:
+        print(workaround(s))
 
 
 def old_compare_both():
@@ -65,6 +59,85 @@ def old_compare_both():
     # Fails
     round_trip(p2)         # Wrong answer
     round_trip('\n' + p2)  # Returns None
+
+
+
+def replace(m):
+    print(m.group(0))
+    parts = [int(i[2:-1]) for i in m.groups()]
+    a, b, *rest = parts
+    if not rest:
+        assert 0xC2 <= a < 0xE0
+        return chr(b + 0x40 * (a - 0xC2))
+
+    c, *rest = rest
+    if not rest:
+        assert 0xE0 <= a < 0xF0
+        return chr(0x800
+            + (c - 0x80) +
+            + 0x40 * ((b - 0xA0)
+                + 0x40 * (a - 0xE0)
+            )
+        )
+
+    d, = rest
+    assert 0xF0 <= a, f'{a=:x}, {b=:x}, {c=:x}, {d=:x}'
+    return chr(0x010000
+        + (d - 0x80) +
+        0x40 * ((c - 0x80) +
+            0x40 * ((b - 0x90)
+                + 0x40 * (a - 0xF0)
+            )
+        )
+    )
+
+
+def _replace(m):
+    parts = [int(i[2:-1]) for i in m.groups() if i]
+    return chr(guess(parts))
+
+
+DIGIT = r'( & \# [1 2] \d \d; )'
+PAT = re.compile(fr'(?: {DIGIT}? {DIGIT} )? {DIGIT} {DIGIT}', re.VERBOSE)
+
+
+def workaround(s: str):
+    if isinstance(s, str):
+        s = s.encode()
+    html = etree.HTML(s)
+    assert html is not None, 'etree.HTML(s) returned None!'
+
+    t = etree.tostring(html, pretty_print=True, method="html").decode()
+
+    return PAT.sub(replace, t)
+
+
+def _convert(parts):
+    a, b, *rest = parts
+    if not rest:
+        assert 0xC2 <= a < 0xE0
+        return b + 0x40 * (a - 0xC2)
+
+    c, *rest = rest
+    if not rest:
+        assert 0xE0 <= a < 0xF0
+        return (0x800
+            + (c - 0x80) +
+            + 0x40 * ((b - 0xA0)
+                + 0x40 * (a - 0xE0)
+            )
+        )
+
+    assert 0xF0 <= a
+    d, = rest
+    return (0x010000
+        + (d - 0x80) +
+        0x40 * ((c - 0x80) +
+            0x40 * ((b - 0x90)
+                + 0x40 * (a - 0xF0)
+            )
+        )
+    )
 
 
 def compare_both():
@@ -94,9 +167,9 @@ def compare_both():
         0x100000,
         0x100001,
     )
-    cps = range(0x010000, 0x010100)
-
-    cps = range(0x80, 0x014000)
+    cps = range(0x80, 0x110000)
+    # cps = range(0x80, 0x82)
+    cps = range(0x7F0, 0x1000)
 
     def cpt(cp):
         try:
@@ -114,25 +187,28 @@ def compare_both():
 
     import re
 
-    pat = re.compile(r'0x(.*?): ' + r'(?:&#(\d\d\d);){1,4}\n')
+    pat = re.compile(r'0x(.*?): ' + r'((?:&#[12]\d\d;){1,4})')
 
-    diff = -1
+    # pat = r'(?: &\# [12] \d \d; ) {1,4}'
 
+    # m = fr'( ( path )? pat )? ({path}) ({pat})'
+
+    # c2 80 -> 80
+    # c3 80 - 100
+    #
     def rep(m):
-        if True:
-            return m.group(0)
-        nonlocal diff
-
-        n, *parts = m.groups()
-        h = ''.join(hex(int(i))[2:] for i in parts)
-        before, after = int(n, 16), int(h, 16)
-        d2 = before - after
-        if d2 == diff:
-            return ''
-        diff = d2
-        return f'0x{n}: 0x{diff:x}\n'
+        n, parts = m.groups()
+        parts = parts.strip(';').split(';')
+        parts = [int(i[2:]) for i in parts]
+        i = ' '.join(str(p) for p in parts)
+        h = ' '.join(hex(p)[2:] for p in parts)
+        gp = guess(parts)
+        assert gp == int(n, 16), f'{gp} {int(n)}'
+        m2 = PAT.search(m.group(0))
+        return f'0x{n}: {h} {i} {m2.groups()}'
 
     print(pat.sub(rep, s))
+
 
 def rewrite2(path=Path('test-index.html')):
     broken_html = "<html><head><title>test<body><h1>page title</h3>"
@@ -156,6 +232,34 @@ def rewrite_lxml(path):
     else:
         print('bytes', len(source), len(out))
         target.write_bytes(out)
+
+
+def guess(parts):
+    a, b, *rest = parts
+    if not rest:
+        assert 0xC2 <= a < 0xE0
+        return b + 0x40 * (a - 0xC2)
+
+    c, *rest = rest
+    if not rest:
+        assert 0xE0 <= a < 0xF0
+        return (0x800
+            + (c - 0x80) +
+            + 0x40 * ((b - 0xA0)
+                + 0x40 * (a - 0xE0)
+            )
+        )
+
+    assert 0xF0 <= a
+    d, = rest
+    return (0x010000
+        + (d - 0x80) +
+        0x40 * ((c - 0x80) +
+            0x40 * ((b - 0x90)
+                + 0x40 * (a - 0xF0)
+            )
+        )
+    )
 
 
 if __name__ == '__main__':
