3ca0870f00e27efe5dbc56717fe0df71e6aadab9
Tom Ritchford <tom@swirly.com>
2023-02-28 18:22:24 +0100

    Finished dashboard.resume

--- multi/commands/dashboard.py
+++ multi/commands/dashboard.py
@@ -13,12 +13,22 @@ Release v1.0.2:  [date] [message]
 Latest commit:  [date] [message]
 
 """
-
 MSG = 'Automatically update README.md'
 
 
 def resume():
     from .. import resume
+
+    path = REC.path # / 'resume'
+    src = path / 'resume.md'
+    assert src.exists()
+    text = src.read_text()
+    prefix = src.parent / src.stem
+    html = resume.make_html(text, prefix=str(prefix))
+    html_file = prefix.with_suffix('.html')
+    html_file.write_text(html)
+    resume.write_pdf(html, prefix=str(prefix))
+    html_file.unlink()
 
 
 def dashboard():
