f9210d999306db7262838bbcf229526b0702019b
Tom Ritchford <tom@swirly.com>
2023-03-07 13:41:44 +0100

    Split bump_version.py off commands/poetry.py

---
+++ multi/commands/bump_version.py
@@ -0,0 +1,13 @@
+from .. paths import PYPROJECT
+
+
+def bump_version(project, rule_or_version):
+    project.run.poetry('version', rule_or_version)
+    project.reload()
+
+    version = 'v' + project.poetry['version']
+    project.git.commit(f'Update to version {version}', PYPROJECT)
+    project.git('tag', version)
+    project.git('push', '--tag', '--force-with-lease')
+    project.run('gh', 'release', 'create', version, '--generate-notes')
+    project.run.poetry('publish', '--build')
--- multi/commands/poetry.py
+++ multi/commands/poetry.py
@@ -1,18 +1,3 @@
-from .. paths import PYPROJECT
-
-
-def bump_version(project, rule_or_version):
-    project.run.poetry('version', rule_or_version)
-    project.reload()
-
-    version = 'v' + project.poetry['version']
-    project.git.commit(f'Update to version {version}', PYPROJECT)
-    project.git('tag', version)
-    project.git('push', '--tag', '--force-with-lease')
-    project.run('gh', 'release', 'create', version, '--generate-notes')
-    project.run.poetry('publish', '--build')
-
-
 def poetry(project, *argv):
     print(project.name + ':')
     project.run.poetry(*argv)
