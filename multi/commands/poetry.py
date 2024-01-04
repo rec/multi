6de090e077c8a87073daee8c290368d27874a518
Tom Ritchford <tom@swirly.com>
2024-01-04 19:13:24 +0100

    Add poetry.add_fields command

--- multi/commands/poetry.py
+++ multi/commands/poetry.py
@@ -1,4 +1,35 @@
+import requests
+from . import bump_version
+
+
 def poetry(project, *argv):
     print(project.name + ':')
     project.run.poetry(*argv)
     print()
+
+
+def add_fields(project):
+    tool = project.configs.setdefault('tool', {}).setdefault('poetry', {})
+    old_tool = dict(tool)
+
+    tool['repository'] = project.git_url
+    tool['homepage'] = project.git_url
+
+    if _exists(project.doc_url):
+        tool['documentation'] = project.doc_url
+
+    if False or tool != old_tool:
+        project.commit_pyproject('Add fields to [tool.poetry] in pyproject.toml')
+        bump_version.bump_version(project, 'patch')
+        project.p('Done')
+    else:
+        project.p(tool)
+
+
+def _exists(url):
+    try:
+        value = requests.get(url)
+    except Exception:
+        return False
+    else:
+        return value.status_code == 200
